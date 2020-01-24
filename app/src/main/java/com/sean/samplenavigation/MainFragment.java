690a081bc27da83d9255f3e646ebe766d0b5eec5
Sean Lee <seancraft79@gmail.com>
2020-01-24 15:17:36 +0900

    Initial Commit

---
+++ app/src/main/java/com/sean/samplenavigation/MainFragment.java
@@ -0,0 +1,49 @@
+package com.sean.samplenavigation;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainFragment extends Fragment {
+
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                MainFragmentDirections.ActionMainFragmentToSecondFragment action = MainFragmentDirections.actionMainFragmentToSecondFragment("Hello !!!");
+
+//                Navigation.findNavController(view).navigate(R.id.action_mainFragment_to_secondFragment);
+                Navigation.findNavController(view).navigate(action);
+            }
+        });
+    }
+}
